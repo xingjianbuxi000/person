00e0f05aacc1f9f1e8d860ad8073c6a73ed454db
xingjianbuxi000 <122657381+xingjianbuxi000@users.noreply.github.com>
2023-12-10 08:51:15 +0800

    Create bilibili_data.js

---
+++ bilibili_data.js
@@ -0,0 +1,17 @@
+#!name = Bilibili共享会员
+#!desc = 需要启用MitM-over-HTTP/2功能，请严格填写数据，支持iPad端。使用时需启用此插件，禁用则共享会员失效，点击下方链接查看使用说明。
+#!openUrl = https://gitlab.com/isous/loonresource/-/blob/main/Document/Bilibili/README.md
+#!author = 伟人
+#!homepage = https://gitlab.com/isous/loonresource/-/tree/main/Plugin
+#!icon = https://gitlab.com/isous/loonresource/-/raw/main/Icon/Bilibili.png
+#!date = 2023-12-10 00:40:28
+#!input = Bilibili_Authorization
+#!input = Bilibili_User-Agent
+
+[Script]
+http-request ^https?:\/\/(grpc\.biliapi\.net|app\.bilibili\.com)\/bilibili\.app\.playerunite\.v1\.Player\/PlayViewUnite(\?.+)?$ script-path = https://gitlab.com/isous/loonresource/-/raw/main/Script/Bilibili/Bilibili_shared_membership.js, tag = iPhone端共享会员
+# http-request ^https?:\/\/(grpc\.biliapi\.net|app\.bilibili\.com)\/bilibili\.app\.player(unite\.v1\.Player\/PlayViewUnite|online\.v1\.PlayerOnline\/PlayerOnline) script-path = https://gitlab.com/isous/loonresource/-/raw/main/Script/Bilibili/Bilibili_shared_membership.js, tag = iPhone端共享会员
+http-request ^https?:\/\/(grpc\.biliapi\.net|app\.bilibili\.com)\/bilibili\.pgc\.gateway\.player\.v2\.PlayURL\/PlayView$ script-path = https://gitlab.com/isous/loonresource/-/raw/main/Script/Bilibili/Bilibili_shared_membership.js, tag = iPad端共享会员
+  
+[Mitm]
+hostname = grpc.biliapi.net, app.bilibili.com
